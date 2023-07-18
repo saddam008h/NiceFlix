c44013e5ae571cca313869ce3906a6aaeacb896f
unknown <arsal0344@gmail.comm>
2023-07-18 14:36:08 +0500

    add backend

--- client/src/redux/userApi.js
+++ client/src/redux/userApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const userApi = createApi({
   reducerPath: "userApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5005/",
+    baseUrl: "https://niceflix-backend.vercel.app/",
   }),
   tagTypes: ["collection"],
   endpoints: (builder) => ({
